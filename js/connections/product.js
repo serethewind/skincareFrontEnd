e2b1f5ebdfb573f79702e1416ab9a9eaf916c3ad
serethewind <osasereu@gmail.com>
2023-07-10 17:04:36 +0100

    product page loading dynamically

--- js/connections/product.js
+++ js/connections/product.js
@@ -32,3 +32,59 @@ function displayProducts(products) {
     productsContainer.appendChild(productCard);
   });
 }
+
+
+
+fetch('http://localhost:8080/api/health/v1/products')
+  .then(response => response.json())
+  .then(products => {
+    const productListElement = document.getElementById('productList');
+    
+    products.forEach(product => { const productElement = document.createElement('div');
+    productElement.className = 'col-lg-4';
+
+    productElement.innerHTML =  `
+    <div class="col">
+    <div class="card shadow-sm">
+      <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+      <div class="card-body">
+        <p class="card-title fw-bold">${product.productName}</p>
+        <p class="card-text">${product.description}</p>
+        <p class="card-text"># ${product.price}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-primary">Add to cart</button>
+          </div>
+          <small class="text-body-secondary">in stock</small>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      productListElement.appendChild(productElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
