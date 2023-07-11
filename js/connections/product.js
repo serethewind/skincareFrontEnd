717ef2c134c0e9c97a56d462b8949d298d5099ee
serethewind <osasereu@gmail.com>
2023-07-11 10:52:29 +0100

    final changes to front end

--- js/connections/product.js
+++ js/connections/product.js
@@ -1,39 +1,4 @@
-// Fetch product data from the backend API
-fetch('/api/products')
-  .then(response => response.json())
-  .then(data => {
-    // Call the function to display the products
-    displayProducts(data);
-  })
-  .catch(error => {
-    // Handle any errors that occur during the fetch request
-    console.error('Error fetching products:', error);
-  });
-
-// Function to display the products on the page
-function displayProducts(products) {
-  const productsContainer = document.getElementById('productsContainer');
-
-  // Loop through the products array and generate HTML for each product
-  products.forEach(product => {
-    // Create the product card element
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
-
-    // Set the product details
-    productCard.innerHTML = `
-      <img src="${product.image}" alt="${product.name}">
-      <h3>${product.name}</h3>
-      <p>${product.description}</p>
-      <span>Price: ${product.price}</span>
-    `;
-
-    // Append the product card to the products container
-    productsContainer.appendChild(productCard);
-  });
-}
-
-
+const userId = localStorage.getItem('userId');
 
 fetch('http://localhost:8080/api/health/v1/products')
   .then(response => response.json())
@@ -42,6 +7,7 @@ fetch('http://localhost:8080/api/health/v1/products')
     
     products.forEach(product => { const productElement = document.createElement('div');
     productElement.className = 'col-lg-4';
+    // localStorage.setItem('productId', product.productId);
 
     productElement.innerHTML =  `
     <div class="col">
@@ -53,7 +19,7 @@ fetch('http://localhost:8080/api/health/v1/products')
         <p class="card-text"># ${product.price}</p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-primary">Add to cart</button>
+            <button type="button" class="btn btn-sm btn-primary" onclick="addToCart('${product.productId}')">Add to cart</button>
           </div>
           <small class="text-body-secondary">in stock</small>
         </div>
@@ -62,26 +28,6 @@ fetch('http://localhost:8080/api/health/v1/products')
   </div>
 `;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
       productListElement.appendChild(productElement);
     });
   })
